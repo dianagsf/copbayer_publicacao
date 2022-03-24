17766786ad08d6cb5a55964f20f0bab8444dad50
dianagsf <dianagama18@gmail.com>
2022-03-24 11:18:31 -0300

    app copbayer publicacao atual

--- android/app/src/main/kotlin/com/copbayer_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/copbayer_app/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.copbayer_app
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-   override fun configureFlutterEngine(@NonNull flutterEngine: 
-     FlutterEngine) {
-     GeneratedPluginRegistrant.registerWith(flutterEngine)
-   }
+class MainActivity: FlutterActivity() {
 }
